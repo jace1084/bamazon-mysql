852e1d7d7c7a363ed02069e5152b46fecae45f53
jace <jace.sf84@gmail.com>
2018-08-10 21:10:39 -0700

    created table and first two colums

--- bamazon.sql
+++ bamazon.sql
@@ -1,3 +1,22 @@
+-- Creating the Database:
+
 DROP DATABASE IF EXISTS bamazon_db;
 CREATE DATABASE bamazon_db;
 USE bamazon_db;
+
+-- Creating the table 'Products':
+
+CREATE TABLE products (
+id INT NOT NULL AUTO_INCREMENT,
+name VARCHAR(255) NOT NULL,
+PRIMARY KEY (id)
+);
+
+-- Creating the colums:
+
+INSERT INTO products (id) VALUES ('Andrew Jackson');
+INSERT INTO products_id (name) VALUES ('');
+
+
+
+
