eeafdff58e449fe8b9de13ce0a2c43d96e5f3334
jace <jace.sf84@gmail.com>
2018-08-10 21:13:17 -0700

    added initial js connection

---
+++ bamayonCustomer.js
@@ -0,0 +1,15 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+// Define the MySQL connection parameters
+var connection = mysql.createConnection({
+	host: 'localhost',
+	port: 3306,
+
+	// Your username
+	user: 'root',
+
+	// Your password
+	password: 'password',
+	database: 'Bamazon'
+});
