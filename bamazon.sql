bdc25b2369e471859b4f5a8fc6cbac7e9ff5caaa
jace <jace.sf84@gmail.com>
2018-08-12 20:25:03 -0700

    changed products table to include more rows

--- bamazon.sql
+++ bamazon.sql
@@ -7,9 +7,12 @@ USE bamazon_db;
 -- Creating the table 'Products':
 
 CREATE TABLE products (
-id INT NOT NULL AUTO_INCREMENT,
-name VARCHAR(255) NOT NULL,
-PRIMARY KEY (id)
+ItemID MEDIUMINT AUTO_INCREMENT NOT NULL,
+ProductName VARCHAR(100) NOT NULL,
+DepartmentName VARCHAR(50) NOT NULL,
+Price DECIMAL(10,2) NOT NULL,
+StockQuantity INT(10) NOT NULL,
+primary key(ItemID)
 );
 
 -- Creating the colums:
