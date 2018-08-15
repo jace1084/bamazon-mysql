7231cf43d6b79ace5ce77a9a9c313521999c6024
jace <jace.sf84@gmail.com>
2018-08-14 20:17:59 -0700

    started the first start function and loop over

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -13,3 +13,19 @@ var connection = mysql.createConnection({
 	password: 'password',
 	database: 'Bamazon'
 });
+
+function start(){
+	//prints the items for sale and their details
+	connection.query('SELECT * FROM Products', function(err, res){
+	  if(err) throw err;
+	
+	  console.log("Welcome to THE Gucci Men's BAMazon")
+	  console.log('|-------------------------------------------~~~---------------------------------------------------------|')
+	
+	  for(var i = 0; i<res.length;i++){
+		console.log("ID: " + res[i].ItemID + " | " + "Product: " + res[i].ProductName + " | " + "Department: " + 
+		res[i].DepartmentName + " | " + "Price: " + res[i].Price + " | " + "QTY: " + res[i].StockQuantity);
+		console.log('|-----------------------------------------~~~---------------------------------------------------------|')
+	  }
+	
+	 
