fc9157240ce53632b3ccde6d5175eb57aa4d61f0
jace <jace.sf84@gmail.com>
2018-08-14 22:17:19 -0700

    fixed line 48 typo

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -45,7 +45,7 @@ function start(){
 		{
 		  type: "input",
 		  name: "qty",
-		  message: "How much would you like to purchase?",
+		  message: "How much many you like to purchase?",
 		  validate: function(value){
 			if(isNaN(value)){
 			  return false;
