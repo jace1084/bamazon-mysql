507c0b915f2b4a7c2867ea362f0829c8ec8ea90c
jace <jace.sf84@gmail.com>
2018-08-14 20:18:55 -0700

    first input queries

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -28,4 +28,30 @@ function start(){
 		console.log('|-----------------------------------------~~~---------------------------------------------------------|')
 	  }
 	
-	 
+	  console.log(' ');
+	  inquirer.prompt([
+		{
+		  type: "input",
+		  name: "id",
+		  message: "What is the ID of the product you would like to purchase?",
+		  validate: function(value){
+			if(isNaN(value) == false && parseInt(value) <= res.length && parseInt(value) > 0){
+			  return true;
+			} else{
+			  return false;
+			}
+		  }
+		},
+		{
+		  type: "input",
+		  name: "qty",
+		  message: "How much would you like to purchase?",
+		  validate: function(value){
+			if(isNaN(value)){
+			  return false;
+			} else{
+			  return true;
+			}
+		  }
+		}
+		
